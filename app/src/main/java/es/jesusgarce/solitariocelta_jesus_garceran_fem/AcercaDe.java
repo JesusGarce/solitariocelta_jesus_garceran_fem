b0c3025086f4ea808b8dce201a98cf8c9a0fadda
jesusgarce <tarraga.jesus@gmail.com>
2019-10-17 18:16:19 +0200

    Initial commit

---
+++ app/src/main/java/es/jesusgarce/solitariocelta_jesus_garceran_fem/AcercaDe.java
@@ -0,0 +1,12 @@
+package es.jesusgarce.solitariocelta_jesus_garceran_fem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
